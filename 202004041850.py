62ddb356feb9b5b3ce6176a2f4543ea93788c1f7
Sibeit <1123640734@qq.com>
2020-04-04 22:13:13 +0800

    up

--- 202004041850.py
+++ 202004041850.py
@@ -1,7 +1,7 @@
 '''
 @Author: Sibeit
 @Date: 2020-04-04 18:49:35
-@LastEditTime: 2020-04-04 21:51:35
+@LastEditTime: 2020-04-04 22:05:34
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: \Python\202004041850.py
@@ -19,24 +19,36 @@ class Student(object):
         if self.score>other.score:
             return True
     def __str__(self):
-        return "姓名"+str(self.name)+"分数"+str(self.score)
+        return "姓名："+str(self.name)+"分数："+str(self.score)
 class Sort(object):
     @staticmethod
     def List(list):
         n=len(list)
         for i in range(n-1):
             for j in range(n-i-1):
-                if list[j] > list[j+1]:
+                if list[j] < list[j+1]:
                     k=list[j+1]
                     list[j+1]=list[j]
                     list[j]=k
 
 
 stulist=[]
-stulist.append(Student("reimu:",82))
+stulist.append(Student("reimu",69))
 stulist.append(Student("momiji",74))
 stulist.append(Student("cirno",99))
+stulist.append(Student("koishi",51))
+
 
 Sort.List(stulist)
+for i in stulist:
+    print(i)
+
+print("————————————————————————————————————")
+print("删除学生koishi")
+print("————————————————————————————————————")
+
+for j in range(len(stulist)):
+    if stulist[j]==Student("koishi",51):
+        del stulist[j]
 for i in stulist:
     print(i)
