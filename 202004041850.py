efa87a373d1bf6aac7edd0aba54fce0427792eab
Sibeit <1123640734@qq.com>
2020-04-04 21:03:57 +0800

    up

--- 202004041850.py
+++ 202004041850.py
@@ -2,7 +2,7 @@ class Student(object):
     def __init__(self,name,score):
         self.name=name
         self.score=score
-    def __eq__(self,other)
+    def __eq__(self,other):
         if self.name==other.name and self.score==other.score:
             return True
         else:
@@ -10,20 +10,24 @@ class Student(object):
     def __gt__(self,other):
         if self.score>other.score:
             return True
+    def __str__(self):
+        return "姓名"+str(self.name)+"分数"+str(self.score)
 class Sort(object):
     @staticmethod
-    def List(list)
+    def List(list):
         n=len(list)
-            while n>0:
-                for i in range(n-1):
-                    if list[i] > list[i+1]:
-                    list[i], list[i+1] = list[i+1], list[i] 
-                n-=1
+        for i in range(n-1):
+            for j in range(n-i-1):
+                if list[j] > list[j+1]:
+                    k=list[j+1]
+                    list[j+1]=list[j]
+                    list[j]=k
+
 
 stulist=[]
-stulist.append(Student(reimu,82))
-stulist.append(Student(momiji,74))
-stulist.append(Student(cirno,99))
-Sort.list(stulist)
+stulist.append(Student("reimu:",82))
+stulist.append(Student("momiji",74))
+stulist.append(Student("cirno",99))
+Sort.List(stulist)
 for i in stulist:
     print(i)
