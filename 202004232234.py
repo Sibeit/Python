a19944725339e4e56ed7a748d1d90110a7d87f45
Sibeit <1123640734@qq.com>
2020-05-04 21:55:20 +0800

    up

--- 202004232234.py
+++ 202004232234.py
@@ -1,15 +1,15 @@
 '''
 @Author: your name
 @Date: 2020-04-23 22:34:53
-@LastEditTime: 2020-04-23 23:36:51
+@LastEditTime: 2020-04-24 00:21:51
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: \Python\202004232234.py
 '''
 def test(li):
     try:
-        n1=li[0]
-        n2=li[1]
+        n1=int(li[0])
+        n2=int(li[1])
         res = n1/n2
         print(res)
     except TypeError as e:
@@ -21,9 +21,15 @@ def test(li):
     except ZeroDivisionError as e:
         print(e)
     else:
-        print("正常运行")
+        print("运算正常")
 
-
-a=int(input("第一个数："))
-b=int(input("第二个数："))
-test([a,b])
+b = open("test.txt", "rt")
+i=[]
+while True:
+    nu=b.readline().strip("\n")
+    print(nu)
+    if nu=="":
+        b.close
+        break
+    i.append(nu)
+test(i)
